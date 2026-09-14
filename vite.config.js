ab90356f09660e39ce4dc5256f4d014a09f5ca82
Santiago Arrua Espinola <santiago.arrua@fiuni.edu.py>
2026-09-13 21:42:20 -0300

    finish?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/Mini-erp-frontend-SantiagoArrua/'
+  plugins: [react()],
+  base: '/Mini-erp-frontend-SantiagoArrua/',
 })
+ 
